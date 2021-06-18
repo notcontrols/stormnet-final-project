ff8450f7bb07043eee9c4d6c13885864a98d9d15
Vladislav <sem.fisher.1997@gmail.com>
2021-06-18 16:31:35 +0300

    add all blocks to home

--- src/components/Blocks/ContactForm/index.js
+++ src/components/Blocks/ContactForm/index.js
@@ -11,8 +11,74 @@ class ContactFrom extends React.Component {
           <div className='gradientLine'></div>
           <div className='subTitle'>
             Whether you're a local or national business, big brand or high
-            profile celebrity, we’ve worked with them all – we can help you get
-            where you want to be.
+            profile celebrity, we’ve worked with <br /> them all – we can help
+            you get where you want to be.
+          </div>
+        </div>
+        <div className='contact_form-wrapper'>
+          <form
+            name=''
+            action='/'
+            method='post'
+            className='home_form'
+            novalidate='novalidate'
+          >
+            <input
+              type='text'
+              name='your-name'
+              value=''
+              className='home_form-control home_form-text'
+              aria-required='true'
+              aria-invalid='false'
+              placeholder='Name'
+            />
+
+            <input
+              type='email'
+              name='your-email'
+              value=''
+              className='home_form-control home_form-text home_form-email'
+              aria-required='true'
+              aria-invalid='false'
+              placeholder='Email'
+            />
+
+            <input
+              type='tel'
+              name='your-tel'
+              value=''
+              className='home_form-control home_form-text home_form-tel'
+              aria-required='true'
+              aria-invalid='false'
+              placeholder='Phone'
+            />
+              <input
+                type='text'
+                name='your-subject'
+                value=''
+                className='home_form-control wpcf7-text'
+                aria-required='true'
+                aria-invalid='false'
+                placeholder='Company Name'
+              />
+            <select
+              name='menu'
+              className='home_form-control home_form-select'
+              aria-required='true'
+              aria-invalid='false'
+            >
+              <option value='Your Budget'>Your Budget</option>
+              <option value='100$'>100$</option>
+              <option value='200$'>200$</option>
+            </select>
+            <input
+              type='submit'
+              value='GET STARTED NOW'
+              className='home_form-submit'
+            />
+          </form>
+          <div className='prompt_form'>
+            <p>All field are required</p>
           </div>
         </div>
       </div>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faLinkedinIn, faTwitter, faVimeoV, faPinterestP } from '@fortawesome/free-brands-svg-icons'
+import {
+  faFacebookF,
+  faLinkedinIn,
+  faTwitter,
+  faVimeoV,
+  faPinterestP,
+} from '@fortawesome/free-brands-svg-icons';
 
+import logo from './img/Logo.png';
 
 import './style.css';
 
@@ -9,9 +16,57 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
+        <div className='footer_wrapper'>
+          <div className='footer_contacts'>
+            <div className='widget_wrap'>
+              <div className='widget widget_text'>
+                <div className='footer_logo'>
+                <img src={logo} alt={'logo'} className='logo' />
+                <p>THEFOX PLUS</p>
+                MADE FOR BUSINESS
+                </div>
+              </div>
+            </div>
+            <div className='widget_wrap'>
+              <div className='widget widget_text'>
+                <h2>Postal Address</h2>
+                  <address>
+                    PO Box 16122 Collins Street <br />
+                    West<br />
+                    Victoria 8007<br />
+                    Australia
+                  </address>
+              </div>
+            </div>
+            <div className='widget_wrap'>
+              <div className='widget widget_text'>
+                <h2>Envato Headquarters</h2>
+                <address>
+                    121 King Street, <br />
+                    Melbourne <br />
+                    Victoria 3000 <br />
+                    Australia
+                  </address>
+              </div>
+            </div>
+            <div className='widget_wrap'>
+              <div className='widget widget_text'>
+                <h2>Tranmautritam</h2>
+                <address>
+                    <a href="mailto:Tranmautritam@gmail.com">Tranmautritam@gmail.com</a><br />
+                    <a href="tel:+84935815989"> +84 935 815 989 </a> <br />
+                    <a href="http://www.tranmautritam.com">tranmautritam.com</a><br />
+                    Vietnam
+                  </address>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='footer_links'>
         <div className='copyright'>
-          © 2015 All Rights Reserved <span className='tranmautritam'>Tranmautritam</span> - Envato Pty Ltd. Contact the
-          Envato Market Help Team.
+          © 2015 All Rights Reserved
+          <a className='tranmautritam' href="http://www.tranmautritam.com"> Tranmautritam </a> - Envato Pty Ltd.
+          Contact the Envato Market Help Team.
         </div>
         <div className='social'>
           <a href=''>
@@ -29,6 +84,7 @@ class Footer extends React.Component {
           <a href=''>
             <FontAwesomeIcon icon={faPinterestP} />
           </a>
+        </div>
         </div>
       </footer>
     );
